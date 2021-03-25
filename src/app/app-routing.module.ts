e18603e4be42eeeb81b296169505c9fda1a4d9f7
agrishurtado <agrishurtado@gmail.com>
2021-03-25 13:01:39 -0600

    se agrega page login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,18 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+  path: 'login',
+  loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+},
+
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
     path: 'acces',
@@ -131,6 +136,9 @@ const routes: Routes = [
     path: 'posts-public',
     loadChildren: () => import('./posts-public/posts-public.module').then( m => m.PostsPublicPageModule)
   },
+ 
+ 
+
 ];
 
 @NgModule({

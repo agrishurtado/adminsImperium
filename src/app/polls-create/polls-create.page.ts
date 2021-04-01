b4207576955b1ba269643cb0b862c7930c989336
agrishurtado <agrishurtado@gmail.com>
2021-04-01 14:01:25 -0600

    se agrega funcion ocultar ver mas a las encuestas sin funcion de eso

--- src/app/polls-create/polls-create.page.ts
+++ src/app/polls-create/polls-create.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { Datos } from '../models/datos.model';
 
 @Component({
   selector: 'app-polls-create',
@@ -6,21 +9,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./polls-create.page.scss'],
 })
 export class PollsCreatePage implements OnInit {
+  post = {} as Datos;
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore,
+              private router: Router) { }
 
   ngOnInit() {
   }
 
-  vermas(parrafo,boton){
-    document.getElementById(parrafo).style.display
-    if(document.getElementById(parrafo).style.display=='enable'){
-      document.getElementById(parrafo).style.display=='disable';
-      document.getElementById(boton).innerHTML=='Ver mas';
-    }else{
-      document.getElementById(parrafo).style.display=='enable';
-      document.getElementById(boton).innerHTML=='ocultar';
+async crear (){
+  try {
+    if(this.formValidation()){
+    await this.firestore.collection("encuesta").add(this.post);
     }
-  }
 
+  } catch (error) {
+    console.log('Error: al crear encuesta');
+  }
+}
+
+formValidation(){
+  if(!this.post.nombre){
+    console.log("Ingresa un Título para tu Encuesta");
+    return false;
+  }
+  if(!this.post.fecha){
+    console.log("Ingresa un fecha de inicio");
+    return false;
+  }
+  if(!this.post.fechaf){
+    console.log("Ingresa un fecha de fin");
+    return false;
+  }
+  if(!this.post.mensaje){
+    console.log("Ingresa un fecha de inicio");
+    return false;
+  }
+  if(!this.post.pregunta1){
+    console.log("Ingresa un fecha de inicio");
+    return false;
+  }
+  if(!this.post.respuesta1){
+    console.log("Ingresa un fecha de inicio");
+    return false;
+  }
+  if(!this.post.respuesta2){
+    console.log("Ingresa un fecha de inicio");
+    return false;
+  }
+}
 }
